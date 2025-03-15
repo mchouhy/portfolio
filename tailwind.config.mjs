1b24c2e41ef37d2a51166030cde0f9b5325aa848
mchouhy <marianochouhy@gmail.com>
2025-03-15 23:15:45 +0100

    fix breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {},
-		screens: {
-			xxs: '320px',
-			xs: '630px', 
-		}
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    screens: {
+      xxs: "320px",
+      xs: "630px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
+  plugins: [],
+};
